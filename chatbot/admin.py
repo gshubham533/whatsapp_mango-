23cd99419a4c08a976bc371ab2c8dfae0207ec1a
Shubham Gupta <gshubham533@gmail.com>
2025-04-09 16:25:36 +0530

    fix: registered models in admin panel

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Messages,Sellers,Contacts,Products,Orders,Peti
 # Register your models here.
+admin.site.register(Messages)
+admin.site.register(Sellers)
+admin.site.register(Contacts)
+admin.site.register(Products)
+admin.site.register(Orders)
+admin.site.register(Peti)
+
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import *
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-
-
+admin.site.register(Staff)
+admin.site.register(CustomUser)
 class UserModel(UserAdmin):
     list_display = ['username','user_type']
